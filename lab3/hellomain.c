82d439f4a84a787888572bb06e4c0784f61bcdbb
jonathantzh <jtan202@e.ntu.edu.sg>
2017-09-20 16:48:13 +0800

    added lab3 stuff

---
+++ lab3/hellomain.c
@@ -0,0 +1,11 @@
+#include "hello.h"
+
+int main() {
+	helloprint();
+	return 0;
+}
+
+/*void helloprint(){
+	printf("hello world!\n");
+}*/
+
---
+++ lab3/part2/mod_hello.c
@@ -0,0 +1,18 @@
+#include <linux/init.h>   /* needed by the macros module_init and exit */ 
+#include <linux/module.h> /* needed by all modules */ 
+#include <linux/kernel.h> /* needed by KERN_ definition*/ 
+static int __init hello_init(void) 
+{ 
+  printk(KERN_ALERT "Hello from kernel world\n"); 
+  return 0; /*0 for success, negative for failure */ 
+} 
+Static void __exit hello_exit(void)  
+{ 
+  printk(KERN_ALERT "Goodbye from kernel world\n"); 
+} 
+module_init(xxxx); /*macro to execute module's initialize routine */ 
+module_exit(xxxx); /*macro to execute module's exit routine */ 
+MODULE_LICENSE("GPL"); 
+MODULE_AUTHOR("CE4052"); 
+MODULE_DESCRIPTION("Simple Hello module");  
+MODULE_VERSION("V1");  
